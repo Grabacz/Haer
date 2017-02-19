a45ae28a2bdd3393d07aa15766fc171bf8fe546b
Grabacz <dariusz.grabacz@gmail.com>
2017-02-19 23:33:23 +0100

    Fix: add ContactsComparer.cs file

---
+++ DG.Haer.Tests/Infastructure/ContactsComparer.cs
@@ -0,0 +1,20 @@
+﻿using DG.Haer.Domain;
+using System.Collections;
+
+namespace DG.Haer.Tests
+{
+    public class ContactsComparer : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            return Compare((Contact)x, (Contact)y);
+        }
+
+        public int Compare(Contact x, Contact y)
+        {
+            if (x.Id == y.Id && x.Name == y.Name && x.Experience == y.Experience && x.Salary == y.Salary && x.ContactType == y.ContactType)
+                return 0;
+            return x.Id > y.Id ? 1 : -1;
+        }
+    }
+}
